e49a796a95099cd430c55701c0b3dbb6d39f1b81
flaviano-faria <flsilv89@yahoo.com.br>
2026-01-20 22:23:23 -0300

    Join CompletableFuture objects and execute parallel calling

--- src/main/java/com/javaasync/Main.java
+++ src/main/java/com/javaasync/Main.java
@@ -1,5 +1,7 @@
 package com.javaasync;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.Executors;
@@ -7,7 +9,7 @@ import java.util.concurrent.Future;
 
 public class Main {
 
-    public static Future<String> calculateAsync() throws InterruptedException {
+    public static Future<String> responseAsync() throws InterruptedException {
         CompletableFuture<String> completableFuture = new CompletableFuture<>();
 
         Executors.newCachedThreadPool().submit(() -> {
@@ -18,10 +20,37 @@ public class Main {
 
         return completableFuture;
     }
+
+
+    public static Future<String> messageAsync() throws InterruptedException {
+        CompletableFuture<String> completableFuture = new CompletableFuture<>();
+
+        Executors.newCachedThreadPool().submit(() -> {
+            Thread.sleep(7000);
+            completableFuture.complete("message");
+            return null;
+        });
+
+        return completableFuture;
+    }
+
     public static void main(String[] args) throws InterruptedException, ExecutionException {
 
-        Future<String> completableFuture = calculateAsync();
-        String response = completableFuture.get();
-        System.out.println(response);
+        Future<String> responseAsyncFuture = responseAsync();
+        Future<String> messageAsyncFuture = messageAsync();
+
+        CompletableFuture<Void> combinedFuture = CompletableFuture.allOf(
+                (CompletableFuture<?>) responseAsyncFuture, (CompletableFuture<?>) messageAsyncFuture);
+
+        CompletableFuture<List<String>> allResults = combinedFuture.thenApply(v ->
+                Arrays.asList(((CompletableFuture<String>) responseAsyncFuture).join(), ((CompletableFuture<String>) messageAsyncFuture).join())
+        );
+
+        try {
+            List<String> results = allResults.get();
+            System.out.println(results);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
