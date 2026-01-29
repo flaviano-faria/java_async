376768175d64842f5a1be78d31631861ef551b71
flaviano-faria <flsilv89@yahoo.com.br>
2026-01-28 22:08:08 -0300

    Including new async method

--- src/main/java/com/javaasync/Main.java
+++ src/main/java/com/javaasync/Main.java
@@ -9,6 +9,21 @@ import java.util.concurrent.Future;
 
 public class Main {
 
+    public static Future<Integer> calcRate(){
+        CompletableFuture<Integer> completableFuture = new CompletableFuture<>();
+
+        Executors.newCachedThreadPool().submit(() -> {
+            try {
+                Thread.sleep(5000);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+            completableFuture.complete(10);
+        });
+
+        return completableFuture;
+    }
+
     public static CompletableFuture<Integer> responseSupplyAsync(){
         int value = 10;
         return CompletableFuture.supplyAsync(() -> value)
@@ -52,22 +67,25 @@ public class Main {
         Future<String> responseAsyncFuture = responseAsync();
         Future<String> messageAsyncFuture = messageAsync();
         CompletableFuture<Integer> messageSupplyAsyncFuture = responseSupplyAsync();
+        CompletableFuture<Integer> calcRateFuture = (CompletableFuture<Integer>) calcRate();
 
         CompletableFuture<Void> combinedFuture = CompletableFuture.allOf(
                 (CompletableFuture<?>) responseAsyncFuture, 
                 (CompletableFuture<?>) messageAsyncFuture,
-                messageSupplyAsyncFuture);
+                messageSupplyAsyncFuture,
+                calcRateFuture
+                );
 
         CompletableFuture<List<Object>> allResults = combinedFuture.thenApply(v ->
                 Arrays.asList(
                         ((CompletableFuture<String>) responseAsyncFuture).join(),
                         ((CompletableFuture<String>) messageAsyncFuture).join(),
-                        messageSupplyAsyncFuture.join())
-        );
+                        messageSupplyAsyncFuture.join(),
+                        (calcRateFuture.join())));
 
         try {
             List<Object> results = allResults.get();
-            System.out.println(results);
+            System.out.println(combinedFuture);
         } catch (Exception e) {
             e.printStackTrace();
         }
